54dfc6f061631ba201950baa7f1f16592f94d4eb
Soufiane <05soufiane@gmail.com>
2026-01-25 00:55:11 +0100

    ddfgd

--- src/terraform/modules/security-group/main.tf
+++ src/terraform/modules/security-group/main.tf
@@ -10,13 +10,13 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "HTTP (phpMyAdmin)"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
-  }
+#   ingress {
+#     description = "HTTP (phpMyAdmin)"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.my_ip}/32"]
+#   }
 
   ingress {
     description = "phpMyAdmin"
